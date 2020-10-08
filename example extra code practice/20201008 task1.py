ac39015d978e2da58f301ab43152062d5ff716cf
Billy Huang <42191587+morganaxmu@users.noreply.github.com>
2020-10-08 15:58:19 +0100

    update task1

--- example extra code practice/20201008 task1.py
+++ example extra code practice/20201008 task1.py
@@ -3,10 +3,16 @@ def dure(L):
     proxy = set()
     for i in L:
         proxy.add(i)
-    return list(proxy)
+    print(list(proxy))
 
 L1 = [3,7,11,-2,7,-7,1,13,7,2,3,1]
 L2 = ["hi","Rob","Steven","hello","hello world","hi world","world","Rob","hello","hello Steven"]
 L3 = [2,"hi",-2,"2",0,"=","Rob","0",2.0,0]
 
 dure(L1)
+
+def dr(L):
+    output = []
+    [output.append(i) for i in L if i not in output]
+    print(output)
+dr(L1)
